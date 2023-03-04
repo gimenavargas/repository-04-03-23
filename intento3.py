533f629ea38a8e44001b24840f1805946b83e94f
gimenavargas <1471366@senati.pe>
2023-03-04 11:17:51 -0500

    imprimir en python el primer y ultimo valor

---
+++ intento3.py
@@ -0,0 +1,5 @@
+list =["red","black","pink","purple","white"]
+n=list[0]
+n=list[-1]
+print(n)
+print(n)
---
+++ new pg .py
@@ -0,0 +1,8 @@
+n=(input("ingresar valores"))
+
+intList=tuple(n)
+print("List = ", intList)
+
+
+intTuple = tuple(n)
+print("Tuple = ", intTuple)
